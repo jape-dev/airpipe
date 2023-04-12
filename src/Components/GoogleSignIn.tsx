92a4c41eee378dd624027890a288464d921469db
James Patten <jamespatten1996@gmail.com>
2023-04-12 21:15:55 +0100

    hardcoded api urls for connectors

--- src/Components/GoogleSignIn.tsx
+++ src/Components/GoogleSignIn.tsx
@@ -1,8 +1,7 @@
 import { useRef, useEffect } from "react";
 declare const google: any;
 
-const DOMAIN_URL =
-  process.env.REACT_APP_DOMAIN_URL || "https://api-airpipe.com";
+const DOMAIN_URL = "https://api-airpipe.com";
 
 const GoogleSignIn = () => {
   const g_sso = useRef(null);
