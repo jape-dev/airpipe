4e536a3ad1c86f6a5ec3948b5c048c6a4338a5fe
James Patten <jamespatten1996@gmail.com>
2023-05-17 09:09:51 +0100

    updated redirect uri for facebook login

--- src/Components/FacebookConnectorV2.tsx
+++ src/Components/FacebookConnectorV2.tsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react";
 import { BaseConnector } from "./BaseConnector";
 import { User } from "../vizoApi";
 
-// const DOMAIN_URL = process.env.REACT_APP_DOMAIN_URL || "http://localhost:8000";
-const DOMAIN_URL = "https://api-airpipe.com";
+const DOMAIN_URL = process.env.REACT_APP_DOMAIN_URL || "http://localhost:8000";
 
 export const FacebookConnectorV2 = (props: { currentUser?: User }) => {
   const [connected, setConnected] = useState(false);
 
   const handleConnect = () => {
     const token = localStorage.getItem("token");
+    const redirect_uri = DOMAIN_URL.replace("www.", "");
     window.location.href = `https://www.facebook.com/v15.0/dialog/oauth?client_id=3796703967222950&redirect_uri=${DOMAIN_URL}/connector/facebook/login/&config_id=728465868571401&state=${token}`;
   };
 
