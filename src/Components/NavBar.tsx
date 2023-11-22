7756b25b7ec8e10b239e0d3932007758ad5f12f5
James Patten <jamespatten1996@gmail.com>
2023-11-22 16:37:07 +0000

    updated logo in navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -20,15 +20,21 @@ export const NavBar = () => {
     };
   }, []);
 
+  const getLogoUrl = (logo: string) => {
+    return require(`../Static/images/${logo}.svg`);
+  };
+
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-teal-500">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container px-4 mx-2 flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link to={RouterPath.HOME}>
-              <p className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white">
-                AirPipe BETA
-              </p>
+              <img
+                className="h-35 w-100 mr-4"
+                src={getLogoUrl("logo_white")}
+                alt="icon"
+              />
             </Link>
           </div>
           {isMobile && (
