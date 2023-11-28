312c2f088936eebd4041f30d489d068de7cc7f8d
James Patten <jamespatten1996@gmail.com>
2023-11-28 18:09:51 +0000

    updated login and signup screens

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -35,37 +35,40 @@ export const Login = () => {
       });
   };
 
+  const getLogoUrl = (logoUrl: string) => {
+    return require(`../Static/images/${logoUrl}.png`);
+  };
+
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          AirPipe
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <img
+          src={getLogoUrl("logo")}
+          className="mb-3 h-auto w-auto"
+          alt="AirPipe logo"
+        />
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Sign in to your account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
                   Your email
                 </label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   required={true}
                   onChange={handleEmailChange}
                 ></input>
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
                   Password
                 </label>
                 <input
@@ -73,21 +76,21 @@ export const Login = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   required={true}
                   onChange={handlePasswordChange}
                 ></input>
               </div>
               <button
                 type="submit"
-                // className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="bg-teal-500 hover:bg-teal-600 mt-5 text-white text-lg rounded-md px-4 py-2 h-16 w-40 flex items-center justify-center mx-auto disabled:opacity-50"
               >
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light text-gray-500">
                 Don’t have an account yet?{" "}
                 <Link
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-600 hover:underline"
                   to={RouterPath.SIGNUP}
                 >
                   {" "}
--- src/Screens/SignUp.tsx
+++ src/Screens/SignUp.tsx
@@ -14,6 +14,11 @@ export const SignUp = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [privacyChecked, setPrivacyChecked] = useState<boolean>(false);
+
+  const getLogoUrl = (logoUrl: string) => {
+    return require(`../Static/images/${logoUrl}.png`);
+  };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -64,35 +69,34 @@ export const SignUp = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          AirPipe
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <img
+          src={getLogoUrl("logo")}
+          className="mb-3 h-auto w-auto"
+          alt="AirPipe logo"
+        />
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Create an account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
                   Your email
                 </label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   onChange={handleEmailChange}
                 />
               </div>
               <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
                   Password
                 </label>
                 <input
@@ -100,24 +104,68 @@ export const SignUp = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   onChange={handlePasswordChange}
                 />
               </div>
+              <p className="text-sm font-light text-gray-500">
+                By signing up, you agree to accept our{" "}
+                <a
+                  href="https://useairpipe.com/privacy"
+                  target="_blank"
+                  className="underline"
+                >
+                  Privacy Policy
+                </a>{" "}
+                and{" "}
+                <a
+                  href="https://useairpipe.com/terms-of-service"
+                  target="_blank"
+                  className="underline"
+                >
+                  Terms of Service.
+                </a>
+              </p>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    <input
+                      id="privacy"
+                      aria-describedby="privacy"
+                      type="checkbox"
+                      className="w-4 h-4 border mt-0 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 align-middle" // Added align-middle here
+                      onChange={() => setPrivacyChecked(!privacyChecked)}
+                    />
+                  </div>
+                  <label
+                    htmlFor="privacy"
+                    className="ml-3 text-sm font-light text-gray-500 align-middle" // Added align-middle here
+                  >
+                    I accept the{" "}
+                    <a
+                      className="font-medium text-primary-600 hover:underline"
+                      href="https://useairpipe.com/privacy"
+                      target="_blank"
+                    >
+                      Privacy Policy
+                    </a>
+                  </label>
+                </div>
+              </div>
+
               <button
                 type="submit"
-                // className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="bg-teal-500 hover:bg-teal-600 mt-5 text-white rounded-md px-4 py-2 h-16 w-60 flex items-center justify-center mx-auto disabled:opacity-50"
+                disabled={!privacyChecked}
               >
-                Create an account
+                Create account
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                By signing up, you agree to be contacted via email. You may
-                unsubscribe at anytime.
-              </p>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+
+              <p className="text-sm font-light text-gray-500">
                 Already have an account?{" "}
                 <Link
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-600 hover:underline"
                   to={RouterPath.LOGIN}
                 >
                   {" "}
